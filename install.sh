503a5cd882b11c3e7995a48478ff6380cf7f35aa
joshuaboud <46575851+joshuaboud@users.noreply.github.com>
2019-03-08 14:36:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-g++ -std=c++11 cephfssyncd.cpp -o /usr/bin/cephgeorepd
+echo Building executable and copying to /usr/bin/ as cephfssyncd
 
-cp cephgeorepd.service /etc/systemd/system/
+g++ -std=c++11 cephfssyncd.cpp -o /usr/bin/cephfssyncd
+
+echo Copying service file to /etc/systemd/system/
+
+cp cephfssyncd.service /etc/systemd/system/
+
+echo Done. Please configure daemon in /etc/ceph/cephfssyncd.conf
+
+read -p "Open config file now? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    nano /etc/ceph/cephfssyncd.conf
+fi
