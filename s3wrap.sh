a2ab592356bcf84325eb4079e5407b130b678c94
Josh Boudreau <joshuaboud@hotmail.com>
2019-11-12 15:06:34 -0400

    fixed gawk FS to have regex to always split at '/./'

--- s3wrap.sh
+++ s3wrap.sh
@@ -4,9 +4,9 @@ BUCKET=$1 # first argument is s3 bucket
 shift
 
 for ARG in "$@"; do
-	REL=$(echo $ARG | awk -F '/./' '{print $2}')
+	REL=$(echo "$ARG" | gawk -F '[/][.][/]' '{print $2}')
 	# echo "s3cmd put $ARG s3://"$BUCKET"/"$REL""
-	s3cmd put $ARG s3://"$BUCKET"/"$REL"
+	s3cmd put "$ARG" s3://"$BUCKET"/"$REL"
 done
 
 exit 0
