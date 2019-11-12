68f1e50b3cac33751cded58f77b20ece4f3e4e42
Josh Boudreau <joshuaboud@hotmail.com>
2019-11-12 10:15:28 -0400

    Added wrapper script for putting files to s3 bucket

---
+++ s3wrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+BUCKET=$1 # first argument is s3 bucket
+shift
+
+for ARG in "$@"; do
+	if [ ! -z "$ARG" ]; then
+		REL=$(echo $ARG | awk -F '/./' '{print $2}')
+		echo "s3cmd put $ARG s3://"$BUCKET"/"$REL""
+		s3cmd put $ARG s3://"$BUCKET"/"$REL"
+	fi
+done
+
+exit 0
