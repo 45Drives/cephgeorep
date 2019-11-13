e85b648a3e5c847354cfc16cab73736940607f29
Josh Boudreau <joshuaboud@hotmail.com>
2019-11-13 12:13:36 -0400

    added exit code handling to s3wrap.sh

--- s3wrap.sh
+++ s3wrap.sh
@@ -7,6 +7,11 @@ for ARG in "$@"; do
 	REL=$(echo "$ARG" | gawk -F '[/][.][/]' '{print $2}')
 	# echo "s3cmd put $ARG s3://"$BUCKET"/"$REL""
 	s3cmd put "$ARG" s3://"$BUCKET"/"$REL"
+	EXIT_CODE=$?
+	if [ $EXIT_CODE -ne 0 ]; then
+		echo s3cmd exited with code $EXIT_CODE
+		exit $EXIT_CODE
+	fi
 done
 
 exit 0
