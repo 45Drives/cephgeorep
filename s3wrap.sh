efc811ed2dce8b3633b4af246fac6af9483c5a79
Josh Boudreau <46575851+joshuaboud@users.noreply.github.com>
2019-11-21 13:16:52 -0400

    fixed regex

--- s3wrap.sh
+++ s3wrap.sh
@@ -4,7 +4,7 @@ BUCKET=$1 # first argument is s3 bucket
 shift
 
 for ARG in "$@"; do
-	REL=$(echo "$ARG" | gawk -F '[/][.][/]' '{print $2}')
+	REL=$(echo "$ARG" | gawk -F '(/\\./)' '{print $2}')
 	# echo "s3cmd put $ARG s3://"$BUCKET"/"$REL""
 	s3cmd put "$ARG" s3://"$BUCKET"/"$REL"
 	EXIT_CODE=$?
