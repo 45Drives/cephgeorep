b9037ed2663afb75deaef4fb60c0770b02ed6658
joshuaboud <joshuaboud@hotmail.com>
2021-01-13 13:10:23 -0400

    make crawler object oriented

--- src/crawler.cpp
+++ src/crawler.cpp
@@ -30,118 +30,214 @@
 
 namespace fs = boost::filesystem;
 
-void sigint_hdlr(int signum){
-	switch(signum){
-		case SIGTERM:
-		case SIGINT:
-			// cleanup from termination
-			writeLast_rctime(last_rctime);
-			exit(EXIT_SUCCESS);
-		default:
-			exit(signum);
-	}
+Crawler::Crawler(fs::path config_path) : config_(config_path, overrides){
+	last_rctime_ = LastRctime(config_.last_rctime_path);
+	payload_bytes_ = 0;
+	base_path_ = config_.base_path_;
 }
 
-void initDaemon(void){
-	loadConfig();
-	Log("Starting Ceph Georep Daemon.",1);
-	if(config.log_level >= 2) dumpConfig();
-	// enable signal handlers to save last_rctime
-	signal(SIGINT, sigint_hdlr);
-	signal(SIGTERM, sigint_hdlr);
-	signal(SIGQUIT, sigint_hdlr);
-	
-	last_rctime = loadLast_rctime();
-	
-	if(!exists(config.sender_dir)) error(SND_DIR_DNE);
+void Crawler::reset(void){
+	file_list_.clear();
+	payload_bytes_ = 0;
 }
 
-void pollBase(fs::path path, bool loop){
-	timespec rctime;
-	std::list<fs::path> sync_queue;
-	Log("Watching: " + path.string(),1);
-	
+void Crawler::pollBase(bool loop){
+	timespec new_rctime;
+	Log("Watching: " + base_path_.string(),1);
 	do{
 		auto start = std::chrono::system_clock::now();
-		if(checkForChange(path, last_rctime, rctime)){
+		if(last_rctime_.checkForChange(base_path_, new_rctime)){
+			Log("Change detected in " + base_path.string(), 1);
 			uintmax_t total_bytes = 0;
-			Log("Change detected in " + path.string(), 1);
-			// create snapshot
-			fs::path snapPath = takesnap(rctime);
-			// wait for rctime to trickle to root
-			std::this_thread::sleep_for(std::chrono::milliseconds(config.prop_delay_ms));
-			// launch crawler in snapshot
-			Log("Launching crawler",2);
-			crawler(snapPath, sync_queue, snapPath, total_bytes); // enqueue if rctime > last_rctime
-			// log list of new files
-			if(config.log_level >= 2){ // skip loop if not logging
-				Log("Files to sync:",2);
-				for(auto i : sync_queue){
-					Log(i.string(),2);
-				}
-			}
-			Log("New files to sync: "+std::to_string(sync_queue.size())+".",1);
+			fs::path snap_path = takesnap(new_rctime);
+			trigger_search(snap_path, total_bytes);
+			Log("New files to sync: "+std::to_string(file_list_.size()),1);
 			// launch rsync
-			if(!sync_queue.empty()){
-				launch_procs(sync_queue, config.rsync_nproc, total_bytes);
+			if(!file_list_.empty()){
+				// TODO: make Syncer class and call sync method here
+				//launch_procs(sync_queue, config.rsync_nproc, total_bytes);
 			}
 			// clear sync queue
-			sync_queue.clear();
+			reset();
 			// delete snapshot
-			deletesnap(snapPath);
+			deletesnap(snap_path);
 			// overwrite last_rctime
-			last_rctime = rctime;
+			last_rctime_.update(rctime);
 		}
 		auto end = std::chrono::system_clock::now();
-		std::chrono::duration<double> elapsed = end-start;
-		if((int)elapsed.count() < config.sync_frequency && loop) // if it took longer than sync freq, don't wait
-			std::this_thread::sleep_for(std::chrono::seconds(config.sync_frequency - (int)elapsed.count()));
+		std::chrono::seconds elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+		if(elapsed.count() < config.sync_frequency && loop) // if it took longer than sync freq, don't wait
+			std::this_thread::sleep_for(std::chrono::seconds(config.sync_frequency) - elapsed.count());
 	}while(loop);
 	writeLast_rctime(last_rctime);
 }
 
-void crawler(fs::path path, std::list<fs::path> &queue, const fs::path &snapdir, uintmax_t &total_bytes){
-	for(fs::directory_iterator itr{path}; itr != fs::directory_iterator{}; *itr++){
-		if((config.ignore_hidden == true &&
-			(*itr).path().filename().string().front() == '.') ||
-			(config.ignore_win_lock  == true &&
-			(*itr).path().filename().string().substr(0,2) == "~$") ||
-			(get_rctime(*itr) < last_rctime))
-			continue;
-		if(is_directory(*itr)){
-			crawler(path/((*itr).path().filename()), queue, snapdir, total_bytes); // recurse
-		}else{
-			// cut path at sync dir for rsync /sync_dir/.snap/snapshotX/./rel_path/file
-			total_bytes += fs::file_size((*itr).path());
-			queue.emplace_back(snapdir/fs::path(".")/fs::relative((*itr).path(),snapdir));
-		}
-	}
-}
-
-bool checkForChange(const fs::path &path, const timespec &last_rctime, timespec &rctime){
-	bool change = false;
-	timespec temp_rctime;
-	for(fs::directory_iterator itr(path); itr != fs::directory_iterator(); *itr++){
-		if((temp_rctime = get_rctime(*itr)) > last_rctime){
-			change = true;
-			if(temp_rctime > rctime) rctime = temp_rctime; // get highest
-		}
-	}
-	return change;
-}
-
-fs::path takesnap(const timespec &rctime){
+fs::path Crawler::create_snap(const timespec &rctime){
 	std::string pid = std::to_string(getpid());
-	fs::path snapPath = fs::path(config.sender_dir).append(".snap/"+pid+"snapshot"+rctime);
+	fs::path snapPath = fs::path(base_path_).append(".snap/"+pid+"snapshot"+rctime);
 	Log("Creating snapshot: " + snapPath.string(), 2);
 	fs::create_directories(snapPath, ec);
 	if(ec) error(PATH_CREATE, ec);
 	return snapPath;
 }
 
-bool deletesnap(fs::path path){
-	Log("Removing snapshot: " + path.string(), 2);
-	bool result = fs::remove(path, ec);
-	if(ec) error(REMOVE_SNAP, ec);
-	return result;
+void Crawler::trigger_search(const fs::path &snap_path, uintmax_t &total_bytes){
+	// wait for rctime to trickle to root
+	std::this_thread::sleep_for(std::chrono::milliseconds(config_.prop_delay_ms));
+	// launch crawler in snapshot
+	Log("Launching crawler",2);
+	// seed recursive function with snap_path
+	find_new_files_recursive(snap_path, snap_path, total_bytes);
+	// log list of new files
+	if(config_.log_level >= 2){ // skip loop if not logging
+		Log("Files to sync:",2);
+		for(auto i : sync_queue){
+			Log(i.string(),2);
+		}
+	}
 }
+
+bool Crawler::ignore_entry(const fs::directory_entry &entry){
+	return (config.ignore_hidden == true && entry.path().filename().string().front() == '.')
+		|| (config.ignore_win_lock  == true && entry.path().filename().string().substr(0,2) == "~$")
+		|| !last_rctime_.is_newer(entry);
+}
+
+void Crawler::find_new_files_recursive(fs::path current_path, const fs::path &snap_root, uintmax_t &total_bytes){
+	for(fs::directory_iterator itr{path}; itr != fs::directory_iterator{}; *itr++){
+		fs::directory_entry entry = *itr;
+		if(ignore_entry(entry)) continue;
+		if(is_directory(entry)){
+			find_new_files_recursive(current_path/(entry.path().filename()), snap_root, total_bytes); // recurse
+		}else{
+			// cut path at sync dir for rsync /sync_dir/.snap/snapshotX/./rel_path/file
+			total_bytes += fs::file_size(entry.path());
+			file_list_.emplace_back(snap_root/fs::path(".")/fs::relative(entry.path(), snap_root));
+		}
+	}
+}
+
+void Crawler::delete_snap(const fs::path &path){
+	Log("Removing snapshot: " + path.string(), 2);
+	boost::system::error_code ec;
+	fs::remove(path, ec);
+	if(ec) error(REMOVE_SNAP, ec);
+}
+
+// -----------------------------
+
+// void sigint_hdlr(int signum){
+// 	switch(signum){
+// 		case SIGTERM:
+// 		case SIGINT:
+// 			// cleanup from termination
+// 			writeLast_rctime(last_rctime);
+// 			exit(EXIT_SUCCESS);
+// 		default:
+// 			exit(signum);
+// 	}
+// }
+// 
+// void initDaemon(void){
+// 	loadConfig();
+// 	Log("Starting Ceph Georep Daemon.",1);
+// 	if(config.log_level >= 2) dumpConfig();
+// 	// enable signal handlers to save last_rctime
+// 	signal(SIGINT, sigint_hdlr);
+// 	signal(SIGTERM, sigint_hdlr);
+// 	signal(SIGQUIT, sigint_hdlr);
+// 	
+// 	last_rctime = loadLast_rctime();
+// 	
+// 	if(!exists(config.sender_dir)) error(SND_DIR_DNE);
+// }
+// 
+// void pollBase(fs::path path, bool loop){
+// 	timespec rctime;teensy lc mounting bracket
+// 	std::list<fs::path> sync_queue;
+// 	Log("Watching: " + path.string(),1);
+// 	
+// 	do{
+// 		auto start = std::chrono::system_clock::now();
+// 		if(checkForChange(path, last_rctime, rctime)){
+// 			uintmax_t total_bytes = 0;
+// 			Log("Change detected in " + path.string(), 1);
+// 			// create snapshot
+// 			fs::path snapPath = takesnap(rctime);
+// 			// wait for rctime to trickle to root
+// 			std::this_thread::sleep_for(std::chrono::milliseconds(config.prop_delay_ms));
+// 			// launch crawler in snapshot
+// 			Log("Launching crawler",2);
+// 			crawler(snapPath, sync_queue, snapPath, total_bytes); // enqueue if rctime > last_rctime
+// 			// log list of new files
+// 			if(config.log_level >= 2){ // skip loop if not logging
+// 				Log("Files to sync:",2);
+// 				for(auto i : sync_queue){
+// 					Log(i.string(),2);
+// 				}
+// 			}
+// 			Log("New files to sync: "+std::to_string(sync_queue.size())+".",1);
+// 			// launch rsync
+// 			if(!sync_queue.empty()){
+// 				launch_procs(sync_queue, config.rsync_nproc, total_bytes);
+// 			}
+// 			// clear sync queue
+// 			sync_queue.clear();
+// 			// delete snapshot
+// 			deletesnap(snapPath);
+// 			// overwrite last_rctime
+// 			last_rctime = rctime;
+// 		}
+// 		auto end = std::chrono::system_clock::now();
+// 		std::chrono::duration<double> elapsed = end-start;
+// 		if((int)elapsed.count() < config.sync_frequency && loop) // if it took longer than sync freq, don't wait
+// 			std::this_thread::sleep_for(std::chrono::seconds(config.sync_frequency - (int)elapsed.count()));
+// 	}while(loop);
+// 	writeLast_rctime(last_rctime);
+// }
+// 
+// void crawler(fs::path path, std::list<fs::path> &queue, const fs::path &snapdir, uintmax_t &total_bytes){
+// 	for(fs::directory_iterator itr{path}; itr != fs::directory_iterator{}; *itr++){
+// 		if((config.ignore_hidden == true &&
+// 			(*itr).path().filename().string().front() == '.') ||
+// 			(config.ignore_win_lock  == true &&
+// 			(*itr).path().filename().string().substr(0,2) == "~$") ||
+// 			(get_rctime(*itr) < last_rctime))
+// 			continue;
+// 		if(is_directory(*itr)){
+// 			crawler(path/((*itr).path().filename()), queue, snapdir, total_bytes); // recurse
+// 		}else{
+// 			// cut path at sync dir for rsync /sync_dir/.snap/snapshotX/./rel_path/file
+// 			total_bytes += fs::file_size((*itr).path());
+// 			queue.emplace_back(snapdir/fs::path(".")/fs::relative((*itr).path(),snapdir));
+// 		}
+// 	}
+// }
+// 
+// bool checkForChange(const fs::path &path, const timespec &last_rctime, timespec &rctime){
+// 	bool change = false;
+// 	timespec temp_rctime;
+// 	for(fs::directory_iterator itr(path); itr != fs::directory_iterator(); *itr++){
+// 		if((temp_rctime = get_rctime(*itr)) > last_rctime){
+// 			change = true;
+// 			if(temp_rctime > rctime) rctime = temp_rctime; // get highest
+// 		}
+// 	}
+// 	return change;
+// }
+// 
+// fs::path takesnap(const timespec &rctime){
+// 	std::string pid = std::to_string(getpid());
+// 	fs::path snapPath = fs::path(config.sender_dir).append(".snap/"+pid+"snapshot"+rctime);
+// 	Log("Creating snapshot: " + snapPath.string(), 2);
+// 	fs::create_directories(snapPath, ec);
+// 	if(ec) error(PATH_CREATE, ec);
+// 	return snapPath;
+// }
+// 
+// bool deletesnap(fs::path path){
+// 	Log("Removing snapshot: " + path.string(), 2);
+// 	bool result = fs::remove(path, ec);
+// 	if(ec) error(REMOVE_SNAP, ec);
+// 	return result;
+// }
--- src/crawler.hpp
+++ src/crawler.hpp
@@ -19,24 +19,83 @@
 
 #pragma once
 
+#include "config.hpp"
+#include "rctime.hpp"
+#include <list>
 #include <boost/filesystem.hpp>
 
 namespace fs = boost::filesystem;
 
-void initDaemon(void);
-// calls loadConfig(), enables signal handlers, asserts that path to sync exists
+class Crawler{
+private:
+	Config config_;
+	/* holds user configuration options
+	 */
+	std::list<fs::path> file_list_;
+	/* lsit of files to send to remote backup
+	 */
+	LastRctime last_rctime_;
+	/* timestamp of last sync
+	 */
+	fs::path base_path_;
+public:
+	Crawler(fs::path config_path, ConfigOverrides overrides);
+	/* calls config constructor with
+	 * config_path and overrides,
+	 * initialises last_rctime_ and payload_bytes_
+	 */
+	~Crawler(void) = default;
+	/* default destructor
+	 */
+	void reset(void);
+	/* clear file_list_ and set
+	 * payload_bytes_ to 0
+	 */
+	void poll_base(bool loop);
+	/* main loop of program
+	 * polls for change in root sync path,
+	 * if change detected, a snapshot is
+	 * taken and the file queuing 
+	 * search is triggered
+	 */
+	fs::path create_snap(const timespec &rctime) const;
+	/* create snapshot in base directory,
+	 * return snapshot path
+	 */
+	void trigger_search(const fs::path &path, uintmax_t &total_bytes);
+	/* queues newly modified/created files
+	 * into file_list_, keeps tally of filesize in
+	 * total_bytes
+	 */
+	bool ignore_entry(const fs::directory_entry &entry) const;
+	/* returns true if file should not be queued or directory should
+	 * not be recursed into
+	 */
+	void find_new_files_recursive(fs::path current_path, const fs::path &snap_root, uintmax_t &total_bytes);
+	/* recursive DFS on directory tree to queue files,
+	 * keeps tally of filesize in total_bytes
+	 */
+	void delete_snap(const fs::path &snap_root) const;
+	/* deletes snapshot directory
+	 */
+};
 
-void pollBase(fs::path path, bool loop);
-// main loop, check for change in rctime and launch crawler
+// ----------------
 
-void crawler(fs::path path, std::list<fs::path> &queue, const fs::path &snapdir, uintmax_t &total_bytes);
-// recursive directory crawler. Returns new files as boost::filesystem::path in queue.
-
-bool checkForChange(const fs::path &path, const timespec &last_rctime, timespec &rctime);
-// returns true if subdir rctime or file mtime > last_rctime. Updates rctime with highest new rctime.
-
-fs::path takesnap(const timespec &rctime);
-// take Ceph snapshot by creating a .snap/<rctime> directory
-
-bool deletesnap(fs::path path);
-// remove snapshot directory path. Returns true if successful, false if path DNE
+// void initDaemon(void);
+// // calls loadConfig(), enables signal handlers, asserts that path to sync exists
+// 
+// void pollBase(fs::path path, bool loop);
+// // main loop, check for change in rctime and launch crawler
+// 
+// void crawler(fs::path path, std::list<fs::path> &queue, const fs::path &snapdir, uintmax_t &total_bytes);
+// // recursive directory crawler. Returns new files as boost::filesystem::path in queue.
+// 
+// bool checkForChange(const fs::path &path, const timespec &last_rctime, timespec &rctime);
+// // returns true if subdir rctime or file mtime > last_rctime. Updates rctime with highest new rctime.
+// 
+// fs::path takesnap(const timespec &rctime);
+// // take Ceph snapshot by creating a .snap/<rctime> directory
+// 
+// bool deletesnap(fs::path path);
+// // remove snapshot directory path. Returns true if successful, false if path DNE
