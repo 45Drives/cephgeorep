a6df1fd045d0795e4c30977dce206cbc972eca74
joshuaboud <joshuaboud@hotmail.com>
2021-01-18 12:23:57 -0400

    update comments

--- src/crawler.hpp
+++ src/crawler.hpp
@@ -30,56 +30,56 @@ namespace fs = boost::filesystem;
 class Crawler{
 private:
 	Config config_;
-	/* holds user configuration options
+	/* Holds user configuration options.
 	 */
 	std::list<fs::path> file_list_;
-	/* lsit of files to send to remote backup
+	/* List of files to send to remote backup.
 	 */
 	LastRctime last_rctime_;
-	/* timestamp of last sync
+	/* Timestamp of last sync.
 	 */
 	fs::path base_path_;
-	/* local source directory
+	/* Local source directory.
 	 */
 	Syncer syncer;
 public:
 	Crawler(const fs::path &config_path, size_t envp_size, const ConfigOverrides &config_overrides);
-	/* calls config constructor with
+	/* Calls config constructor with
 	 * config_path and overrides,
-	 * initialises last_rctime_ and payload_bytes_
+	 * initialises last_rctime_ and payload_bytes_.
 	 */
 	~Crawler(void) = default;
-	/* default destructor
+	/* Default destructor.
 	 */
 	void reset(void);
-	/* clear file_list_ and set
-	 * payload_bytes_ to 0
+	/* Clear file_list_ and set
+	 * payload_bytes_ to 0.
 	 */
 	void poll_base(bool seed, bool dry_run);
-	/* main loop of program
-	 * polls for change in root sync path,
+	/* Main loop of program.
+	 * Polls for change in root sync path,
 	 * if change detected, a snapshot is
-	 * taken and the file queuing 
-	 * search is triggered
+	 * taken and the file queuing
+	 * search is triggered.
 	 */
 	fs::path create_snap(const timespec &rctime) const;
-	/* create snapshot in base directory,
-	 * return snapshot path
+	/* Create snapshot in base directory,
+	 * return snapshot path.
 	 */
 	void trigger_search(const fs::path &path, uintmax_t &total_bytes);
-	/* queues newly modified/created files
+	/* Queues newly modified/created files
 	 * into file_list_, keeps tally of filesize in
-	 * total_bytes
+	 * total_bytes.
 	 */
 	bool ignore_entry(const fs::directory_entry &entry) const;
-	/* returns true if file should not be queued or directory should
-	 * not be recursed into
+	/* Returns true if file should not be queued or directory should
+	 * not be searched.
 	 */
 	void find_new_files_recursive(fs::path current_path, const fs::path &snap_root, uintmax_t &total_bytes);
-	/* recursive DFS on directory tree to queue files,
-	 * keeps tally of filesize in total_bytes
+	/* Recursive DFS on directory tree to queue files.
+	 * Keeps tally of filesize in total_bytes.
 	 */
 	void delete_snap(const fs::path &snap_root) const;
-	/* deletes snapshot directory
+	/* Deletes snapshot directory.
 	 */
 };
