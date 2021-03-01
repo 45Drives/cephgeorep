1f8c0a101b5c49c33686db95cafa792c05b4d5a9
joshuaboud <joshuaboud@hotmail.com>
2021-03-01 17:06:11 -0400

    set uid and gid for output files

--- create-deb.sh
+++ create-deb.sh
@@ -7,10 +7,24 @@ command -v docker > /dev/null 2>&1 || {
 
 if [[ "$(docker images -q ubuntu-builder 2> /dev/null)" == "" ]]; then
 	docker build -t ubuntu-builder .
+	res=$?
+	if [ $res -ne 0 ]; then
+		echo "building docker image failed."
+		exit $res
+	fi
 fi
 
 mkdir -p dist/ubuntu
 
-docker run -w /home/deb/build -it -v$(pwd):/home/deb/build -v$(pwd)/dist/ubuntu:/home/deb --rm ubuntu-builder dpkg-buildpackage -us -uc -b
+docker run -u $(id -u):$(id -g) -w /home/deb/build -it -v$(pwd):/home/deb/build -v$(pwd)/dist/ubuntu:/home/deb --rm ubuntu-builder dpkg-buildpackage -us -uc -b
+res=$?
+if [ $res -ne 0 ]; then
+	echo "packaging failed."
+	exit $res
+fi
+
+rmdir dist/ubuntu/build
 
 echo "deb is in dist/ubuntu/"
+
+exit 0
