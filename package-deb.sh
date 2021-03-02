c35648b6f62c72d6873b7ba077385bab4147f59f
joshuaboud <joshuaboud@hotmail.com>
2021-03-02 13:02:08 -0400

    cleaned up scripts and added copyright disclaimer

--- package-deb.sh
+++ package-deb.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+#    Copyright (C) 2019-2021 Joshua Boudreau <jboudreau@45drives.com>
+# 
+#    This file is part of cephgeorep.
+# 
+#    cephgeorep is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+# 
+#    cephgeorep is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+# 
+#    You should have received a copy of the GNU General Public License
+#    along with cephgeorep.  If not, see <https://www.gnu.org/licenses/>.
+
 if [[ "$#" == 1 && "$1" == "clean" ]]; then
 	pushd debian
 	rm -f cephgeorep.postrm.debhelper cephgeorep.substvars debhelper-build-stamp files
@@ -9,15 +26,16 @@ if [[ "$#" == 1 && "$1" == "clean" ]]; then
 fi
 
 command -v docker > /dev/null 2>&1 || {
-	echo "please install docker.";
+	echo "Please install docker.";
 	exit 1;
 }
 
+# if docker image DNE, build it
 if [[ "$(docker images -q ubuntu-builder 2> /dev/null)" == "" ]]; then
 	docker build -t ubuntu-builder - < docker/ubuntu
 	res=$?
 	if [ $res -ne 0 ]; then
-		echo "building docker image failed."
+		echo "Building docker image failed."
 		exit $res
 	fi
 fi
@@ -26,10 +44,11 @@ make clean
 
 mkdir -p dist/ubuntu
 
+# mirror current directory to working directory in container, and mirror dist/ubuntu to .. for deb output
 docker run -u $(id -u):$(id -g) -w /home/deb/build -it -v$(pwd):/home/deb/build -v$(pwd)/dist/ubuntu:/home/deb --rm ubuntu-builder dpkg-buildpackage -us -uc -b
 res=$?
 if [ $res -ne 0 ]; then
-	echo "packaging failed."
+	echo "Packaging failed."
 	exit $res
 fi
 
--- package-rpm.sh
+++ package-rpm.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
+#    Copyright (C) 2019-2021 Joshua Boudreau <jboudreau@45drives.com>
+# 
+#    This file is part of cephgeorep.
+# 
+#    cephgeorep is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+# 
+#    cephgeorep is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+# 
+#    You should have received a copy of the GNU General Public License
+#    along with cephgeorep.  If not, see <https://www.gnu.org/licenses/>.
+
 command -v docker > /dev/null 2>&1 || {
-	echo "please install docker.";
+	echo "Please install docker.";
 	exit 1;
 }
 
+# if docker image DNE, build it
 if [[ "$(docker images -q el7-builder 2> /dev/null)" == "" ]]; then
 	docker build -t el7-builder - < docker/centos7
 	res=$?
 	if [ $res -ne 0 ]; then
-		echo "building docker image failed."
+		echo "Building docker image failed."
 		exit $res
 	fi
 fi
@@ -26,7 +44,7 @@ mkdir -p $DEST
 ./docker-make -j8 static
 res=$?
 if [ $res -ne 0 ]; then
-	echo "building failed."
+	echo "Building failed."
 	exit $res
 fi
 
@@ -36,10 +54,11 @@ pushd $DEST/..
 tar -czvf $SOURCE_DIR.tar.gz $SOURCE_DIR
 popd
 
+# build rpm from source tar and place it dist/el7 by mirroring dist/el7 to rpmbuild/RPMS
 docker run -u $(id -u):$(id -g) -w /home/rpm/rpmbuild -it -v$(pwd)/dist/tmp:/home/rpm/rpmbuild/SOURCES -v$(pwd)/dist/el7:/home/rpm/rpmbuild/RPMS -v$(pwd)/centos7:/home/rpm/rpmbuild/SPECS --rm el7-builder rpmbuild -ba SPECS/cephgeorep.spec
 res=$?
 if [ $res -ne 0 ]; then
-	echo "packaging failed."
+	echo "Packaging failed."
 	exit $res
 fi
 
