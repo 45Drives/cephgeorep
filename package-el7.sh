6cd9efa0b0862535d75ced8cce298a036069dd19
joshuaboud <joshuaboud@hotmail.com>
2021-04-07 12:36:03 -0300

    rename centos to el

--- package-el7.sh
+++ package-el7.sh
@@ -30,7 +30,7 @@ command -v docker > /dev/null 2>&1 || {
 
 # if docker image DNE, build it
 if [[ "$(docker images -q cephgeorep-el7-builder 2> /dev/null)" == "" ]]; then
-	docker build -t cephgeorep-el7-builder - < docker/centos7
+	docker build -t cephgeorep-el7-builder - < docker/el7
 	res=$?
 	if [ $res -ne 0 ]; then
 		echo "Building docker image failed."
@@ -42,7 +42,7 @@ make clean
 
 mkdir -p dist/{el7,tmp}
 
-SOURCE_DIR=cephgeorep-$(grep Version centos7/cephgeorep.spec --color=never | awk '{print $2}')
+SOURCE_DIR=cephgeorep-$(grep Version el7/cephgeorep.spec --color=never | awk '{print $2}')
 DEST=dist/tmp/$SOURCE_DIR
 mkdir -p $DEST
 
@@ -61,7 +61,7 @@ tar -czvf $SOURCE_DIR.tar.gz $SOURCE_DIR
 popd
 
 # build rpm from source tar and place it dist/el7 by mirroring dist/el7 to rpmbuild/RPMS
-docker run -u $(id -u):$(id -g) -w /home/rpm/rpmbuild -it -v$(pwd)/dist/tmp:/home/rpm/rpmbuild/SOURCES -v$(pwd)/dist/el7:/home/rpm/rpmbuild/RPMS -v$(pwd)/centos7:/home/rpm/rpmbuild/SPECS --rm cephgeorep-el7-builder rpmbuild -ba SPECS/cephgeorep.spec
+docker run -u $(id -u):$(id -g) -w /home/rpm/rpmbuild -it -v$(pwd)/dist/tmp:/home/rpm/rpmbuild/SOURCES -v$(pwd)/dist/el7:/home/rpm/rpmbuild/RPMS -v$(pwd)/el7:/home/rpm/rpmbuild/SPECS --rm cephgeorep-el7-builder rpmbuild -ba SPECS/cephgeorep.spec
 res=$?
 if [ $res -ne 0 ]; then
 	echo "Packaging failed."
