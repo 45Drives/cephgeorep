a74d2943d3c71180d87dc90dce5e9fc4c08b40b4
joshuaboud <joshuaboud@hotmail.com>
2021-05-18 14:43:33 -0300

    add copyright header

--- src/incl/syncProcess.hpp
+++ src/incl/syncProcess.hpp
@@ -1,3 +1,22 @@
+/*
+ *    Copyright (C) 2019-2021 Joshua Boudreau <jboudreau@45drives.com>
+ *    
+ *    This file is part of cephgeorep.
+ * 
+ *    cephgeorep is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 2 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    cephgeorep is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with cephgeorep.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <vector>
@@ -45,7 +64,7 @@ private:
 	/* Iterator to list of files to sync.
 	 */
 	std::vector<char *> payload_;
-	/* argv for sync process
+	/* argv for sync process.
 	 */
 public:
 	SyncProcess(Syncer *parent, int id, int nproc, std::vector<File> &queue);
